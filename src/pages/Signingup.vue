58d2c6a1b5c00bd78eb9f4f9233d94227975ec9d
user.email <dar.midadari@gmail.com>
2021-08-25 15:40:29 +0800

    Signup changes

--- src/pages/Signingup.vue
+++ src/pages/Signingup.vue
@@ -1,62 +1,88 @@
 <template>
-<q-page class="bg-blue">
-
-
-<div>
-  <h2 class="flex flex-center text-white"  style="weight-bolder"> Signing Page</h2> 
- 
-</div>
-  <div class="q-pa-md">
-    <div class="q-gutter-md" style="max-width: 300px">
-      <q-card class="absolute-center" style="width: 300px">
-        <div class="q-gutter-md">
-            
-          <q-input filled v-model="text" label="FULL NAME" />
-          <q-input filled v-model="text" label="ADDRESS" />
-          <q-input filled v-model="text" label="EMAIL" />
-          <q-input filled v-model="text" label="USERNAME" />
-
-          <q-input
-            v-model="password"
-            filled
-            :type="isPwd ? 'password' : 'text'"
-            label="PASSWORD"
-          >
-            <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
-            </template>
-          </q-input>
-
-      <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" label="PASSWORD">
-        <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          />
-          </template>
-      </q-input>
-          
-        </div>
-
-        <div class="q-pa-md q-gutter-sm flex flex-center">
-          <div>
-            <q-btn
-              color="blue"
-              text-color="white"
-              label="CONFIRM" to="Login"
-              style="width: 250px"
-            />
-          </div>
-         
-        </div>
-      </q-card>
+  <q-page class="bg-blue">
+    <div>
+      <h2 class="flex flex-center text-white" style="weight-bolder">
+        Signing Page
+      </h2>
     </div>
+    <div class="q-pa-md">
+      <div class="q-gutter-md" style="max-width: 300px">
+        <q-card class="absolute-center" style="width: 500px">
+          <div class="q-gutter-md q-pa-md">
+            <q-input filled v-model="Fname" label="FULL NAME" />
+            <q-input filled v-model="ad" label="ADDRESS" />
+            <q-input filled v-model="em" label="EMAIL" />
+            <q-input filled v-model="uname" label="USERNAME" />
+
+            <q-input
+              v-model="password"
+              filled
+              :type="isPwd ? 'password' : 'pass'"
+              label="PASSWORD"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
+              </template>
+            </q-input>
+
+            <q-input
+              v-model="password"
+              filled
+              :type="isPwd ? 'password' : 'pass'"
+              label="PASSWORD"
+            >
+              <template v-slot:append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
+              </template>
+            </q-input>
+          </div>
+
+          <div class="q-pa-md q-gutter-sm flex flex-center">
+            <div>
+              <q-btn
+                color="blue"
+                text-color="white"
+                label="CONFIRM"
+                to="Login"
+                style="width: 250px"
+              />
+             <div class="q-pa-md">
+    <q-btn-dropdown  style="width: 250px" color="blue" label="Select User">
+      <q-list>
+        <q-item clickable v-close-popup>
+          <q-item-section>
+            <q-item-label>Admin</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup>
+          <q-item-section>
+            <q-item-label>Client</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup>
+          <q-item-section>
+            <q-item-label>User</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
   </div>
+            
+            </div>
+          </div>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -66,9 +92,13 @@ import { ref } from 'vue';
 export default {
   data() {
     return {
-      text: '',
-      password:'',
-      isPwd: ref(true),
+      Fname: '',
+      ad: '',
+      em: '',
+      uname: '',
+      pass: '',
+      password: '',
+      isPwd: ref(false),
     };
   },
 };
