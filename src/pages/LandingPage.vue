497d3aa11ad4a82f2623157345b41e75f5541fce
user.email <dar.midadari@gmail.com>
2021-08-26 15:54:32 +0800

    LandingPage Changes

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="bg-blue">
+      <div class="col">
+          <q-card>
+              <img class="col text-center q-pt-md"
+              src="~assets/images/image.jpg"
+              style="height: 500"
+              />
+          </q-card>
+      </div>
+    <div class="text-center text-h3 text-bold text-arial text-brown q-pt-lg">
+      Welcome to the MSU Document Tracking System
+    </div>
+
+    <div class="text-center text-h6 text-arial text-brown q-pa-lg">
+      The Document Tracking System (DTS) of Mindanao State University Main
+      Campus (MSU-Main) is an information system capable of tracking the paper
+      trail of documents created within MSU offices. The DTS includes
+      information on the originating and receiving office and personnel, as well
+      as the time elapsed between departments.
+    </div>
+  </q-page>
+</template>
