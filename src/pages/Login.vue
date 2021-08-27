1bafa8650acfc99fe11ed54d3024a39a960913a3
user.email <dar.midadari@gmail.com>
2021-08-27 14:35:47 +0800

    Login Design

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,14 +1,19 @@
 <template>
   <q-page class="bg-image">
     <div>
-      <h2  class="flex flex-center text-black" style="weight-bolder">
+      <h2 class="flex flex-center text-black" style="weight-bolder">
         DOCUMENT TRACKING SYSTEM
       </h2>
     </div>
-    <div class="q-pa-md" >
+    <div class="q-pa-md">
       <div class="q-gutter-md" style="width: 300px">
-        <q-card class="absolute-center bg-blue-5" style="width: 450px">
+        <q-card class="absolute-center bg-blue-3" style="width: 450px">
           <div class="q-gutter-md q-pa-md">
+            <div>
+              <h4 class="flex flex-center text-black" style="weight-bolder">
+                LOG IN
+              </h4>
+            </div>
             <q-input filled v-model="text" label="Username">
               <template v-slot:prepend>
                 <q-icon name="people" />
@@ -33,39 +38,29 @@
               </template>
             </q-input>
             <div>
-              <div class="q-pa-md">
-                <div class="q-gutter-sm">
-                  <q-checkbox v-model="right" label="Remember my Password" />
-                </div>
+              <div class="q-pa-xs">
+                <q-checkbox v-model="right" label="Remember my Password" />
 
                 <div class="q-pa-md text-center">
-                  <div class="q-px-sm">
-                    Select User As <strong>{{ shape }}</strong>
-                    <div class="q-gutter-xs">
-                      <q-radio v-model="shape" val="Client" label="Client" />
-                      <q-radio
-                        v-model="shape"
-                        val="Employee"
-                        label="Employee"
-                      />
-
-                      <div class="q-pa-md q-gutter-md flex flex-center">
-                        <div>
-                          <q-btn
-                            color="blue-5"
-                            text-color="black"
-                            label="Log In"
-                            style="width: 300px"
-                          />
-                        </div>
-                        <q-btn
+                  Select User As <strong>{{ shape }}</strong>
+                  <div class="q-gutter-xs">
+                    <q-radio v-model="shape" val="Client" label="Client" />
+                    <q-radio v-model="shape" val="Employee" label="Employee" />
+                    <div class="q-pa-md q-gutter-md flex flex-center">
+                      <q-btn
+                        outline
+                        color="deep-orange"
                         text-color="black"
-                          color="blue-5"
-                          label="Sign Up"
-                          to="Signingup"
-                          style="width: 300px"
-                        />
-                      </div>
+                        label="LOG IN"
+                        style="width: 300px"
+                      />
+                      <q-btn
+                        outline
+                        color="deep-blue"
+                        label="SIGN UP"
+                        to="Signingup"
+                        style="width: 300px"
+                      />
                     </div>
                   </div>
                 </div>
