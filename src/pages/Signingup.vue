7d8ac30239f0fc6e5141e965e1777f7ce2d3c1c9
user.email <dar.midadari@gmail.com>
2021-08-27 14:36:10 +0800

    Signingup design

--- src/pages/Signingup.vue
+++ src/pages/Signingup.vue
@@ -1,76 +1,65 @@
 <template>
   <q-page class="bg-image">
     <div>
-      <h2 class=" text-black flex flex-center text-white" style="weight-bolder">
-        SIGN UP PAGE
-      </h2>
+      <h3 class="text-black text-white" style="weight-bolder">SIGN UP here!</h3>
     </div>
     <div class="q-pa-md">
-      <div class="q-gutter-md" style="max-width: 300px">
-        <q-card class="absolute-center bg-blue-5" style="width: 450px">
-          <div class="q-gutter-md q-pa-md">
-            <q-input filled v-model="Fname" label="FULL NAME" />
-            <q-input filled v-model="ad" label="ADDRESS" />
-            <q-input filled v-model="em" label="EMAIL" />
-            <q-input filled v-model="uname" label="USERNAME" />
+      <q-card class="absolute-center bg-blue-3" style="width: 500px">
+        <div class="q-gutter-xs q-pa-md">
+          <q-input filled v-model="Fname" label="FULL NAME" />
+          <q-input filled v-model="em" label="EMAIL" />
+          <q-input filled v-model="uname" label="USERNAME" />
+          <q-select
+            color="black"
+            filled
+            v-model="model"
+            :options="options"
+            label="STATUS"
+          />
 
-            <q-input
-              v-model="pass"
-              filled
-              :type="isPwd ? 'password' : 'pass'"
-              label="PASSWORD"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPwd ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="isPwd = !isPwd"
-                />
-              </template>
-            </q-input>
+          <q-input
+            v-model="pass"
+            filled
+            :type="isPwd ? 'password' : 'pass'"
+            label="PASSWORD"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
 
-            <q-input
-              v-model="password"
-              filled
-              :type="isPwd ? 'password' : 'pass'"
-              label="PASSWORD"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPwd ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="isPwd = !isPwd"
-                />
-              </template>
-            </q-input>
-          </div>
-
-          <div class="q-pa-xs flex flex-center bg-blue-5">
-            <div class="q-pa-md" style="width: 300px">
-              <div class="q-gutter-md bg-blue" >
-                <q-select
-                  color="black" 
-                  filled
-                  v-model="model"
-                  :options="options"
-                  label="STATUS"
-                />
-              </div>
-            </div>
-            <div class="q-pa-xs q-gutter-sm flex flex-center">
-              <div class="flex flex-center">
-                <q-btn
-                  color="blue"
-                  text-color="black"
-                  label="CONFIRM"
-                  to="Login"
-                  style="width: 300px"
-                />
-              </div>
-            </div>
-          </div>
-        </q-card>
-      </div>
+          <q-input
+            v-model="password"
+            filled
+            :type="isPwd ? 'password' : 'pass'"
+            label="CONFIRM PASSWORD"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+        </div>
+        <div class="q-pa-xl q-gutter-md flex flex-center">
+          <q-btn
+            class="glossy"
+            rounded
+            outline
+            color="blue"
+            text-color="black"
+            label="CONFIRM"
+            to="Login"
+            style="width: 250px"
+          />
+        </div>
+      </q-card>
     </div>
   </q-page>
 </template>
@@ -82,7 +71,6 @@ export default {
   data() {
     return {
       Fname: '',
-      ad: '',
       em: '',
       uname: '',
       pass: '',
