5e23eab016a79e9fb0579fe5a4ed876b061914c3
user.email <dar.midadari@gmail.com>
2021-08-30 17:04:33 +0800

    Login Design

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,79 +1,78 @@
 <template>
   <q-page class="bg-image">
-    <div>
-      <h2 class="flex flex-center text-black" style="weight-bolder">
-        DOCUMENT TRACKING SYSTEM
-      </h2>
-    </div>
-    <div class="q-pa-md">
-      <div class="q-gutter-md" style="width: 300px">
-        <q-card class="absolute-center bg-blue-3" style="width: 450px">
-          <div class="q-gutter-md q-pa-md">
-            <div>
-              <h4 class="flex flex-center text-black" style="weight-bolder">
-                LOG IN
-              </h4>
-            </div>
-            <q-input filled v-model="text" label="Username">
-              <template v-slot:prepend>
-                <q-icon name="people" />
-              </template>
-            </q-input>
+    <q-header>
+      <q-toolbar class="bg-white">
+        <q-btn
+          class="absolute-left"
+          flat
+          style="color: #ff0080"
+          label="Sign Up"
+          to="Signingup"
+        />
 
-            <q-input
-              v-model="password"
-              filled
-              :type="isPwd ? 'password' : 'text'"
-              label="Password"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPwd ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="isPwd = !isPwd"
-                />
-              </template>
-              <template v-slot:prepend>
-                <q-icon name="lock" />
-              </template>
-            </q-input>
-            <div>
-              <div class="q-pa-xs">
-                <q-checkbox v-model="right" label="Remember my Password" />
+        <div class="text-h3 text-bold text-broadway text-pink absolute-center">
+          Document Tracking System
+        </div>
+      </q-toolbar>
+    </q-header>
 
-                <div class="q-pa-md text-center">
-                  Select User As <strong>{{ shape }}</strong>
-                  <div class="q-gutter-xs">
-                    <q-radio v-model="shape" val="Client" label="Client" />
-                    <q-radio v-model="shape" val="Employee" label="Employee" />
-                    <div class="q-pa-md q-gutter-md flex flex-center">
-                      <q-btn
-                        outline
-                        color="deep-orange"
-                        text-color="black"
-                        label="LOG IN"
-                        style="width: 300px"
-                      />
-                      <q-btn
-                        outline
-                        color="deep-blue"
-                        label="SIGN UP"
-                        to="Signingup"
-                        style="width: 300px"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <div
+      class="q-gutter-md q-pa-md absolute-center bg-white"
+      style="width: 450px; height: 500px"
+    >
+      <div>
+        <h4 class="flex flex-center text-blue text-bold">
+          Sign into your Account
+        </h4>
+      </div>
+
+      <q-input class="bg-blue-3" filled v-model="text" placeholder="Username">
+        <template v-slot:prepend>
+          <q-icon name="people" />
+        </template>
+      </q-input>
+
+      <q-input
+        class="bg-blue-3"
+        placeholder="Password"
+        v-model="password"
+        filled
+        :type="isPwd ? 'password' : 'text'"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+        <template v-slot:prepend>
+          <q-icon name="lock" />
+        </template>
+      </q-input>
+
+      <div class="q-pa-xs">
+        <q-checkbox v-model="right" label="Remember me" />
+
+        <div class="q-pa-xs text-center">
+          Select User As <strong>{{ shape }}</strong>
+          <div class="q-gutter-xs">
+            <q-radio v-model="shape" val="Client" label="Client" />
+            <q-radio v-model="shape" val="Employee" label="Employee" />
+            <div class="q-pa-lg">
+              <q-btn
+                outline
+                rounded
+                color="primary"
+                label="LOG IN"
+                style="width: 300px"
+              />
             </div>
           </div>
-        </q-card>
+        </div>
       </div>
     </div>
   </q-page>
-  <q-page-container>
-    <router-view />
-  </q-page-container>
 </template>
 <script>
 import { ref } from 'vue';
