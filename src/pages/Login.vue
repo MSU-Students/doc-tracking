b9744fb6009c082c7156d5a8f4b4d4119aa99ce0
user.email <dar.midadari@gmail.com>
2021-09-07 11:41:30 +0800

    Login ChangeDesign

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,96 +1,97 @@
 <template>
-  <q-page class="bg-image">
-    <q-header>
-      <q-toolbar class="bg-white">
-        
 
-        <div class="text-h3 text-bold text-broadway text-pink absolute-center">
-          Document Tracking System
-        </div>
-      </q-toolbar>
-    </q-header>
-
-    <div
-      class="q-gutter-md q-pa-md absolute-center bg-white"
-      style="width: 450px; height: 550px"
-    >
-      <div>
-        <h4 class="flex flex-center text-blue text-bold">
-          Sign into your Account
-        </h4>
-      </div>
-
-      <q-input class="bg-blue-3" filled v-model="text" placeholder="Username">
-        <template v-slot:prepend>
-          <q-icon name="people" />
-        </template>
-      </q-input>
-
-      <q-input
-        class="bg-blue-3"
-        placeholder="Password"
-        v-model="password"
-        filled
-        :type="isPwd ? 'password' : 'text'"
-      >
-        <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          />
-        </template>
-        <template v-slot:prepend>
-          <q-icon name="lock" />
-        </template>
-      </q-input>
-
-      <div class="q-pa-xs">
-        <q-checkbox v-model="right" label="Remember me" />
-
-        <div class="q-pa-xs text-center">
-          Select User As <strong>{{ shape }}</strong>
-          <div class="q-gutter-xs">
-            <q-radio v-model="shape" val="Client" label="Client" />
-            <q-radio v-model="shape" val="Employee" label="Employee" />
-            <div class="q-pa-lg q-gutter-md">
-              <q-btn
-                outline
-                rounded
-                color="primary"
-                label="LOG IN"
-                style="width: 300px"
-             
-              />
-             <q-btn
-                outline
-                rounded
-                color="primary"
-                label="SIGN UP" to="Signingup"
-                style="width: 300px"
-             
-              />
-              
-              
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex bg-image flex-center">
+        <q-card
+          v-bind:style="$q.screen.lt.sm ? { width: '90%' } : { width: '25%' }"
+        >
+          <q-card-section>
+            <q-avatar size="125px" class="absolute-center shadow-10">
+              <img src="../assets/images7.png" />
+            </q-avatar> </q-card-section
+          ><br />
+          <q-card-section>
+            <div class="text-center q-pt-lg">
+              <div class="col text-bold text-h5 ellipsis">DTS</div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </q-page>
-</template>
-<script>
-import { ref } from 'vue';
+          </q-card-section>
+          <q-card-section>
+            <q-form class="q-pa-xs q-gutter-xs">
+              <q-input
+                v-model="username"
+                label="Username"
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Input your Username',
+                ]"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="people" />
+                </template>
+              </q-input>
 
-export default {
-  setup() {
-    return {
-      text: ref(''),
-      password: ref(''),
-      isPwd: ref(true),
-      shape: ref(''),
-      right: ref(true),
-    };
-  },
-};
+              <q-input
+                v-model="password"
+                label="Password"
+                :type="isPwd ? 'password' : 'text'"
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Input your password',
+                ]"
+              >
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                  />
+                </template>
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+              </q-input>
+             
+              <div class="q-pa-md q-gutter-xs">
+                <q-btn
+                class="full-width"
+                outline
+                rounded
+                color="blue-6"
+                label="Login"
+                 @click="loginUser()"
+              />
+              <div class="flex flex-center q-pa-md q-gutter-sm" >
+              <div>Don't have an account?
+                <q-btn align="middle" flat color="primary" label="Sign Up" to="Signingup"/>
+                </div>
+                </div>
+              </div>
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component';
+@Options({})
+export default class Login extends Vue {
+  username = '';
+  password = '';
+  isPwd = true;
+
+  async loginUser() {
+    if (this.username == 'Admin' && this.password == 'Admin') {
+      await this.$router.replace('/Dashboard');
+      this.$q.notify('You are logged in');
+    } else {
+      this.username = '';
+      this.password = '';
+      this.$q.notify('Wrong Username and Password!!');
+    }
+  }
+}
 </script>
+
+
