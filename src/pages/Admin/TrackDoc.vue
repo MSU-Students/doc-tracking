e56ad2ec1b5de2652fe9aae12a66dd9dabd48da2
user.email <dar.midadari@gmail.com>
2021-09-07 11:00:17 +0800

    Add TrackDocPage

---
+++ src/pages/Admin/TrackDoc.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="q-pa-md absolute-center">
+    <div class="q-gutter-y-md column" style="width: 600px">
+     
+      <q-input class="text-italic" filled v-model="ph" placeholder="Enter barcode here" />
+      <q-btn outline style="color: goldenrod;" color="grey-4" text-color="primary" glossy unelevated icon="shopping_cart" label="Track" />
+      
+    
+      
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      text: ref(''),
+      ph: ref(''),
+      dense: ref(false)
+    }
+  }
+}
+</script>
